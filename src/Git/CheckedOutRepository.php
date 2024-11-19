a8138a33b21396885b974383345cbc7791eb76bc
Tobias Nyholm <tobias.nyholm@gmail.com>
2024-11-19 19:39:39 +0100

    allow sub-directories

--- src/Git/CheckedOutRepository.php
+++ src/Git/CheckedOutRepository.php
@@ -7,6 +7,8 @@ namespace Roave\BackwardCompatibility\Git;
 use Psl;
 use Psl\Filesystem;
 
+use function dirname;
+
 /** @psalm-immutable */
 final class CheckedOutRepository
 {
@@ -18,9 +20,17 @@ final class CheckedOutRepository
     /** @param non-empty-string $path */
     public static function fromPath(string $path): self
     {
-        Psl\invariant(Filesystem\is_directory($path . '/.git'), 'Directory "%s" is not a GIT repository.', $path);
+        $testPath = $path;
 
-        return new self($path);
+        do {
+            if (Filesystem\is_directory($testPath . '/.git')) {
+                return new self($path);
+            }
+
+            $testPath = dirname($path);
+        } while ($testPath !== '.');
+
+        Psl\invariant_violation('Directory "%s" is not in a GIT repository.', $path);
     }
 
     /** @return non-empty-string */
